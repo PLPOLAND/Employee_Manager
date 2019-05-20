da7b09ab5be9de9b3e798e0214851ffc933734dc
Marcin Kos <marcin.a380@gmail.com>
2019-05-20 19:49:59 +0200

    - dopasowanie szkieletu do aplikacji internetowej - stworzenie strony logowania

--- src/main/java/PWJ/Employee_Manager/EmployeeManagerApplication.java
+++ src/main/java/PWJ/Employee_Manager/EmployeeManagerApplication.java
@@ -2,12 +2,22 @@ package PWJ.Employee_Manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EmployeeManagerApplication {
+
+public class EmployeeManagerApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(EmployeeManagerApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeManagerApplication.class, args);
+
 	}
 
+	// testuje commit - Marcin
 }
