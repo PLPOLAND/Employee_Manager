36294585204d1aa5011c09d9b302ed7ac62027fd
Marcin Kos <marcin.a380@gmail.com>
2019-05-23 23:18:09 +0200

    Dodanie modelu użytkownika. Dodanie DAO użytkownika. Dodanie ustawień połączenia bazy danych.

---
+++ src/main/java/PWJ/Employee_Manager/dao/UsersDAO.java
@@ -0,0 +1,20 @@
+package PWJ.Employee_Manager.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UsersDAO {
+
+	@Autowired
+	private JdbcTemplate jdbc;
+	
+	
+	
+	
+	// https://www.mkyong.com/spring/spring-jdbctemplate-querying-examples/
+	// https://www.mkyong.com/spring-boot/spring-boot-jdbc-mysql-hikaricp-example/
+	// https://www.codejava.net/coding/jsp-servlet-jdbc-mysql-create-read-update-delete-crud-example
+	
+}
---
+++ src/main/java/PWJ/Employee_Manager/model/User.java
@@ -0,0 +1,87 @@
+package PWJ.Employee_Manager.model;
+
+public class User {
+
+	private int id;
+	private String name;
+	private String surname;
+	private String email;
+	private String account_number;
+	private double gross_salary; // brutto
+	private double net_salary; // netto
+	private String position; // stanowisko
+	private String contract_type;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAccount_number() {
+		return account_number;
+	}
+
+	public void setAccount_number(String account_number) {
+		this.account_number = account_number;
+	}
+
+	public double getGross_salary() {
+		return gross_salary;
+	}
+
+	public void setGross_salary(double gross_salary) {
+		this.gross_salary = gross_salary;
+	}
+
+	public double getNet_salary() {
+		return net_salary;
+	}
+
+	public void setNet_salary(double net_salary) {
+		this.net_salary = net_salary;
+	}
+
+	public String getPosition() {
+		return position;
+	}
+
+	public void setPosition(String position) {
+		this.position = position;
+	}
+
+	public String getContract_type() {
+		return contract_type;
+	}
+
+	public void setContract_type(String contract_type) {
+		this.contract_type = contract_type;
+	}
+
+}
