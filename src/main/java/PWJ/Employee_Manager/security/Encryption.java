2e37b9fc60325c77ee6d0d3d2779bfa7f69aa2c5
Rafi <Rafi@Rafi-Komputer>
2019-06-01 15:11:22 +0200

    Aktualizacja szyfrowania

--- src/main/java/PWJ/Employee_Manager/security/Encryption.java
+++ src/main/java/PWJ/Employee_Manager/security/Encryption.java
@@ -4,6 +4,7 @@ public class Encryption
 {
 	/*Przesunięcie litery według tabeli ASCII, wynik zawsze w przedziale 33-122(od spacji do 'z')*/
 	/**
+	 * Podmienia znak przesuwając go w tablicy ASCII o podany offset w przedziale 33-122 bez 34,39 i 96
 	 * @param character - znak do podmiany
 	 * @param offset - o ile przesunąć znak według tablicy ASCII
 	 * @return changed_character - podmieniony znak po przesunięciu
@@ -16,11 +17,20 @@ public class Encryption
 		while(character>'z')// > 122
 			character = (char) ('!' + (character - 'z') - 1);//(33 + (character - 122) - 1)
 		
+		if(character == 34 || character == 39 || character == 96)
+		{
+			if(offset >0)
+				character++;
+			else 
+				character--;
+		}
+		
 		return character;
 	}
 	
 	/*Algorytm szyfrujący hasło*/
 	/**
+	 * Szyfruje podane hasło
 	 * @param password - hasło do zaszyfrowania
 	 * @return encrypted_paswword - zaszyfrowane hasło
 	 */
@@ -34,6 +44,8 @@ public class Encryption
 			if(i%3==0)
 			{
 				character = changeChar(password.charAt(i), -2);
+				encrypted += character;
+				character = changeChar(password.charAt(i), +1);
 			}
 			else if (i%3==1)
 			{
@@ -42,6 +54,8 @@ public class Encryption
 				character = changeChar(password.charAt(i-1), 9);
 				encrypted += character;
 				character = changeChar(password.charAt(i), 29);
+				encrypted += character;
+				character = changeChar(password.charAt(i), -13);
 			}
 			else
 			{
@@ -50,6 +64,8 @@ public class Encryption
 				character = changeChar(password.charAt(i-1), 4);
 				encrypted += character;
 				character = changeChar(password.charAt(i), 21);
+				encrypted += character;
+				character = changeChar(password.charAt(i), 13);
 			}
 			
 			encrypted += character;
@@ -58,14 +74,9 @@ public class Encryption
 			for(int j=0;j<=encrypted.length()-1;j++)
 			{
 				if(j*i%2 == 0)
-				{
-					new_encryption += changeChar(encrypted.charAt(j), 7+character);
-				}
+					new_encryption += changeChar(encrypted.charAt(j), character);
 				else
-				{
-					new_encryption += changeChar(encrypted.charAt(j), 13-character);
-					new_encryption += changeChar(encrypted.charAt(j), 76+character);
-				}
+					new_encryption += changeChar(encrypted.charAt(j), 93-character);
 			}
 			encrypted = new_encryption;			
 		}
